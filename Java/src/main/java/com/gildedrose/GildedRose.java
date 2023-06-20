36046f71c13b47ef049b5fdf2ad8820c7f978634
Guillaume Dernoncourt <gdernoncourt@cirb.brussels>
2023-06-20 16:06:00 +0200

    refactor(tests): improve readability
    handle Aged Brie

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -20,9 +20,16 @@ class GildedRose {
                 return;
             }
 
+            item.sellIn = item.sellIn - 1;
 
-            if (!item.name.equals("Aged Brie")
-                    && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            if (item.name.equals("Aged Brie")) {
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
+                }
+                return;
+            }
+
+            if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                 if (item.quality > 0) {
                         item.quality = item.quality - 1;
                 }
@@ -46,10 +53,7 @@ class GildedRose {
                 }
             }
 
-            item.sellIn = item.sellIn - 1;
-
             if (item.sellIn < 0) {
-                if (!item.name.equals("Aged Brie")) {
                     if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                         if (item.quality > 0) {
                                 item.quality = item.quality - 1;
@@ -57,11 +61,7 @@ class GildedRose {
                     } else {
                         item.quality = item.quality - item.quality;
                     }
-                } else {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1;
-                    }
-                }
+
             }
         });
     }
